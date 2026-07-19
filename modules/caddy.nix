585a41e82b198413839bf16f9e671b048a515809
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-07-19 16:05:12 +0200

    fix(caddy): update cloudflare plugin hash (#199)
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -15,7 +15,7 @@ in
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-zlBCmHJeeZsRR1OFyoayE/v1WH9n5SPy2JBJkgeHqdc=";
+      hash = "sha256-mqIa0wI/VfjDblg0NnkzKllWHXZZPLwHP8xEVSwZuPE=";
     };
 
     virtualHosts = {
